0c7e5b8410675be6233b08eedc73b3ab1d705161
Dmitry Fedyuk <dfediuk@gmail.com>
2018-05-12 19:06:46 +0600

    no message

---
+++ Header.php
@@ -0,0 +1,31 @@
+<?php
+namespace Dfe\Frugue;
+use Magento\Framework\View\Element\AbstractBlock as _P;
+// 2018-05-12
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Header extends _P {
+	/**
+	 * 2018-05-12
+	 * @override
+	 * @see _P::_toHtml()
+	 * @used-by _P::toHtml():
+	 *		$html = $this->_loadCache();
+	 *		if ($html === false) {
+	 *			if ($this->hasData('translate_inline')) {
+	 *				$this->inlineTranslation->suspend($this->getData('translate_inline'));
+	 *			}
+	 *			$this->_beforeToHtml();
+	 *			$html = $this->_toHtml();
+	 *			$this->_saveCache($html);
+	 *			if ($this->hasData('translate_inline')) {
+	 *				$this->inlineTranslation->resume();
+	 *			}
+	 *		}
+	 *		$html = $this->_afterToHtml($html);
+	 * https://github.com/magento/magento2/blob/2.2.0/lib/internal/Magento/Framework/View/Element/AbstractBlock.php#L643-L689
+	 * @return string
+	 */
+	final protected function _toHtml() {return df_tag('div', 'header-info dfe-frugue-header',
+		df_tag('div', 'container', 'TEST')
+	) /*. df_style_inline_hide('.rd-navbar .header-info')*/;}
+}
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+// 2017-04-28
+// «Replace a default JS component»:
+// http://devdocs.magento.com/guides/v2.0/javascript-dev-guide/javascript/custom_js.html#js_replace
+// 2018-04-18
+// «How to override a HTML file using a custom module?»:
+// https://stackoverflow.com/a/37464758
+// https://magento.stackexchange.com/a/117236
+var config = {
+    config: {
+    	// 2018-04-22 «What are requirejs-config.js `mixins`?» https://mage2.pro/t/5297
+        mixins: {'Magento_Theme/js/jquery.rd-navbar': {'Dfe_Frugue/rd-navbar': true}}
+    }
+};
