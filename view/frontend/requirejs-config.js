34ecceab77e188f0ec9d923b4c91b844f3e43c37
Dmitry Fedyuk <dfediuk@gmail.com>
2018-08-23 11:03:40 +0600

    0.3.8: https://github.com/frugue/core/issues/18

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+// 2018-08-23
+// "Remove the «Ajax Wishlist» heading from the wishlist update confirmation popup"
+// https://github.com/frugue/core/issues/18
+var config = {map: {'*': {
+	'ajaxWishlist': 'Frugue_Core/ajaxwishlist'
+	,'TemplateMonster_AjaxWishlist/js/ajaxwishlist': 'Frugue_Core/ajaxwishlist'
+}}};
