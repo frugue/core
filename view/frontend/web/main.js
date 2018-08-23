7f8a4ae0d8f5c6125c048ed8eec8d8d7232ba3bc
Dmitry Fedyuk <dfediuk@gmail.com>
2018-08-23 13:19:51 +0600

    0.3.9: https://github.com/frugue/core/issues/19

---
+++ view/frontend/web/main.js
@@ -0,0 +1,19 @@
+// 2018-08-23
+require(['jquery'], function($) {$(function() {
+	var $compare = $('.header a.compare');
+	var resize = function() {
+		var w = $(window).width();
+		$('.rd-navbar').children('.layout_1,layout_2,layout_3,layout_4,layout_5').toggleClass(
+			'frugue-navbar-collapse'
+			,w > 768 && w < ('none' == $compare.css('display') ? 992 : 1200)
+		);
+	};
+	$(window).on('resize', resize);
+	resize();
+	// 2018-08-23 https://stackoverflow.com/a/16462443
+	if ('undefined' != typeof MutationObserver) {
+		var observer = new MutationObserver(function() {resize();});
+		var target = document.querySelector('.header a.compare');
+		observer.observe(target, {attributes: true});
+	}
+});});
